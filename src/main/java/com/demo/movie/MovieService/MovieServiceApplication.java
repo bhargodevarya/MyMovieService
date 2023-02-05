5662cfa85016d092279d87933e79f44d7a107872
Bhargodev Arya <bhargodevarya@gmail.com>
2023-02-05 18:58:47 +0530

    Removing CommandLineRunner from main class

--- src/main/java/com/demo/movie/MovieService/MovieServiceApplication.java
+++ src/main/java/com/demo/movie/MovieService/MovieServiceApplication.java
@@ -1,18 +1,12 @@
 package com.demo.movie.MovieService;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieServiceApplication implements CommandLineRunner {
+public class MovieServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieServiceApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.getenv().entrySet().forEach(entry -> System.out.println(entry.getKey() + " " + entry.getValue()));
 	}
 }
